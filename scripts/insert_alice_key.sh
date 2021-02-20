70c94ce63b80aa1a031d8f3b22c077d2c925b85b
wannam2049 <71331352+wannam2049@users.noreply.github.com>
2021-02-21 01:25:10 +0800

    Update insert_alice_key.sh

--- scripts/insert_alice_key.sh
+++ scripts/insert_alice_key.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#Key（密钥）	Value（值）
-#Secret phrase（助记词）	clip organ olive upper oak void inject side suit toilet stick narrow
+#Key	Value
+#Secret phrase	clip organ olive upper oak void inject side suit toilet stick narrow
 #Secret seed	0x4bd2b2c1dad3dbe3fa37dc6ad5a4e32ddd8ad84b938179ac905b0622880e86e7
 #SR25519
-#Public key（公钥）	0x9effc1668ca381c242885516ec9fa2b19c67b6684c02a8a3237b6862e5c8cd7e
+#Public key	0x9effc1668ca381c242885516ec9fa2b19c67b6684c02a8a3237b6862e5c8cd7e
 #SS58 Address	5FfBQ3kwXrbdyoqLPvcXRp7ikWydXawpNs2Ceu3WwFdhZ8W4
 #ED25519
-#Public key（公钥）	0xb48004c6e1625282313b07d1c9950935e86894a2e4f21fb1ffee9854d180c781
+#Public key	0xb48004c6e1625282313b07d1c9950935e86894a2e4f21fb1ffee9854d180c781
 #SS58 Address	5G9NWJ5P9uk7a**4yCKeLZJqXWW6hjuMyRJDmw4ofqxG8Js2
 # insert aura key
 curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -X POST  --data '{
