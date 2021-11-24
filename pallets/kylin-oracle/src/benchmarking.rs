689c1938b266dd32af3e7681eccf8e046a92f81b
Logan Bonsignore <Loganbon14@gmail.com>
2021-11-23 22:16:57 -0600

    add extrinsic benchmarks

--- pallets/kylin-oracle/src/benchmarking.rs
+++ pallets/kylin-oracle/src/benchmarking.rs
@@ -1,84 +1,152 @@
-// //! Benchmarking setup for pallet-kylin-oracle
-// #[cfg(feature = "runtime-benchmarks")]
-// use super::*;
-// use crate as kylin_oracle;
-// use crate::*;
-// use hex::ToHex;
-// #[allow(unused)]
-// use crate::Pallet as Template;
-// use frame_system::RawOrigin;
-// use frame_system::EventRecord;
-
-// pub type BalanceOf<T> =
-// 	<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-	
-
-// /*fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
-// 	let events = frame_system::Pallet::<T>::events();
-// 	let system_event: <T as frame_system::Config>::Event = generic_event.into();
-// 	// compare to the last event record
-// 	let EventRecord { event, .. } = &events[events.len() - 1];
-// 	assert_eq!(event, &system_event);
-// }*/
-
-// benchmarks! {
-// 	where_clause { where 
-// 		T::AccountId: AsRef<[u8]>,	
-// 	}
-// 	submit_data_unsigned {
-// 		let s in 0 .. 100;
-		
-//         let caller: T::AccountId = whitelisted_caller();		
-// 		let benchmarking_test_data = str::from_utf8(b"Testing Benchmark").unwrap().as_bytes().to_vec();
-// 		let block_number = <system::Pallet<T>>::block_number();
-		
-//     }: _(RawOrigin::None, block_number, 18446744073709551615, benchmarking_test_data)      
-// 	verify {
-// 		//assert_eq!(Something::<T>::get(), Some(s));
-// 		//assert_last_event(Event::ResponseSent);
-// 	}
-// }
-
-// #[cfg(test)]
-// mod tests {
-// 	use super::*;
-// 	use crate::tests::{ExtBuilder, Test};
-// 	use frame_Support::assert_ok;
-
-// 	#[test]
-// 	fn submit_data_signed (){
-// 		let mut t = sp_io::TestExternalities::default();
-// 		t.execute_with(||{
-// 			assert_ok!(test_benchmark_submit_data_signed::<Test>())
-// 		})
-// 	}
-
-// }
-
-// impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test,);
-
-
 #![cfg(feature = "runtime-benchmarks")]
 use super::*;
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_system::{EventRecord, RawOrigin};
 
-use frame_system::RawOrigin;
 benchmarks! {
-	where_clause { where 
-		T::AccountId: AsRef<[u8]>,	
+    where_clause { where
+		T::AccountId: AsRef<[u8]>,
 	}
-	submit_data_unsigned {
-		let s in 0 .. 100;
-		
-        let caller: T::AccountId = whitelisted_caller();		
-		let benchmarking_test_data = str::from_utf8(b"Testing Benchmark").unwrap().as_bytes().to_vec();
-		let block_number = <system::Pallet<T>>::block_number();
-		
-    }: _(RawOrigin::None, block_number, 18446744073709551615, benchmarking_test_data)      
-	verify {
-		//assert_eq!(Something::<T>::get(), Some(s));
-		//assert_last_event(Event::ResponseSent);
-	}
+
+    sudo_remove_feed_account {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+		let feed_name = str::from_utf8(b"price_feeding").unwrap().as_bytes().to_vec();
+        let hash: Vec<u8> = Vec::new();
+        <FeedAccountLookup<T>>::insert(feed_name.clone(), (caller.clone(), hash));
+    }: _(RawOrigin::Root, feed_name.clone())
+    verify {
+        assert_last_event::<T>(Event::RemovedFeedAccount(feed_name).into());
+    }
+
+    submit_price_feed { 
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+        let api_url = str::from_utf8(b"https://api.kylin-node.co.uk/prices?currency_pairs=").unwrap();
+        let requested_currencies = str::from_utf8(b"btc_usd,eth_gdp").unwrap();
+        let url = api_url.clone().to_owned() + requested_currencies;
+    }: _(RawOrigin::Signed(caller.clone()), None, requested_currencies.as_bytes().to_vec())
+    verify {
+        let block_number = frame_system::Pallet::<T>::block_number();
+        let feed_name = str::from_utf8(b"price_feeding").unwrap().as_bytes().to_vec();
+        assert_last_event::<T>(Event::SubmitNewData(
+            None,
+            feed_name,
+            Some(url.as_bytes().to_vec()),
+            Some(caller),
+            block_number).into()
+        );
+    }
+
+    // query_data {
+    //     let a in 1 .. 100;
+    //     let caller: T::AccountId = whitelisted_caller();
+    //     let feed_name = str::from_utf8(b"test_feed").unwrap().as_bytes().to_vec();
+    //     let data = b"{'benchmark':'this is data'}".to_vec();
+    //     let _ = Pallet::<T>::write_data_onchain(RawOrigin::Signed(caller.clone()).into(), feed_name.clone(), data);
+    //     let new_feed_name = str::from_utf8(b"custom_").unwrap().to_owned() + str::from_utf8(&feed_name).unwrap();
+    // }: _(RawOrigin::Signed(caller.clone()), None, new_feed_name.as_bytes().to_vec())
+
+    write_data_onchain {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+		let feed_name = str::from_utf8(b"feed_name").unwrap().as_bytes().to_vec();
+        let data = b"{'benchmark':'this is data'}".to_vec();
+    }: _(RawOrigin::Signed(caller.clone()), feed_name.clone(), data)
+    verify {
+        let block_number = frame_system::Pallet::<T>::block_number();
+        let new_feed_name = str::from_utf8(b"custom_").unwrap().to_owned() + str::from_utf8(&feed_name).unwrap();
+        assert_last_event::<T>(Event::SubmitNewData(
+            None,
+            new_feed_name.as_bytes().to_vec(),
+            None,
+            Some(caller.clone()),
+            block_number).into(),
+        );
+    }
+
+    submit_data_signed {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+        let block_number = <system::Pallet<T>>::block_number();
+        let key: u64 = a.into();
+        let data = str::from_utf8(b"{'benchmark':'this is data'}").unwrap().as_bytes().to_vec();
+    }: _(RawOrigin::Signed(caller), block_number, key, data.clone())
+    verify {
+        let saved_request = Pallet::<T>::saved_data_requests(key.clone());
+        assert_eq!(saved_request.payload, data)
+    }
+
+    submit_data_unsigned {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+        let block_number = <system::Pallet<T>>::block_number();
+        let key: u64 = a.into();
+        let data = str::from_utf8(b"{'benchmark':'this is data'}").unwrap().as_bytes().to_vec();
+    }: _(RawOrigin::None, block_number, key, data.clone())
+    verify {
+        let saved_request = Pallet::<T>::saved_data_requests(key.clone());
+        assert_eq!(saved_request.payload, data)
+    }
+
+    submit_data_via_api {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+        let url = str::from_utf8(b"https://min-api.cryptocompare.com/data/price?fsym=btc&tsyms=usd").unwrap().as_bytes().to_vec();
+		let feed_name = str::from_utf8(b"feed_name").unwrap().as_bytes().to_vec();
+    }: _(RawOrigin::Signed(caller.clone()), None, url.clone(), feed_name.clone())
+    verify {
+        let block_number = frame_system::Pallet::<T>::block_number();
+        let new_feed_name = str::from_utf8(b"custom_").unwrap().to_owned() + str::from_utf8(&feed_name).unwrap();
+        assert_last_event::<T>(Event::SubmitNewData(
+            None,
+            new_feed_name.as_bytes().to_vec(),
+            Some(url),
+            Some(caller),
+            block_number).into()
+        );
+    }
+
+    // xcm_submit_data_via_api {
+    //     let a in 1 .. 100;
+    //     let caller: T::AccountId = whitelisted_caller();
+    //     let url = str::from_utf8(b"https://min-api.cryptocompare.com/data/price?fsym=btc&tsyms=usd").unwrap().as_bytes().to_vec();
+	// 	let feed_name = str::from_utf8(b"feed_name").unwrap().as_bytes().to_vec();
+    // }: _(RawOrigin::Root, url, feed_name)
+
+    // receive_response_from_parachain {
+    //     let a in 1 .. 100;
+    //     let caller: T::AccountId = whitelisted_caller();
+	// 	let feed_name = str::from_utf8(b"feed_name").unwrap().as_bytes().to_vec();
+    //     let response = b"{'test':'test'}".to_vec();
+    // }: _(RawOrigin::Root, feed_name, response)
+
+    clear_api_queue_unsigned {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+        let block_number = <system::Pallet<T>>::block_number();
+
+        let key: u64 = a.into();
+        let saved_request = DataRequest {
+            para_id: None,
+            account_id: Some(caller),
+            feed_name: str::from_utf8(b"price_feeding").unwrap().as_bytes().to_vec(),
+            requested_block_number: frame_system::Pallet::<T>::block_number(),
+            processed_block_number: None,
+            requested_timestamp: T::UnixTime::now().as_millis(),
+            processed_timestamp: None,
+            payload: str::from_utf8(b"{'benchmark':'this is data'}").unwrap().as_bytes().to_vec(),
+            is_query: false,
+            url: None,
+        };
+        <ApiQueue<T>>::insert(key.clone(), saved_request.clone());
+        let processed_requests = [key.clone()].to_vec();
+
+    }: _(RawOrigin::None, block_number, processed_requests)
+    verify {
+        let in_queue = ApiQueue::<T>::contains_key(key);
+        assert_eq!(in_queue, false);
+    }
 }
 
 impl_benchmark_test_suite!(
@@ -86,3 +154,12 @@ impl_benchmark_test_suite!(
 	crate::mock::TestExternalitiesBuilder::default().build(|| {}),
 	crate::mock::MockRuntime,
 );
+
+
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) where <T as frame_system::Config>::AccountId: AsRef<[u8]> {
+	let events = frame_system::Pallet::<T>::events();
+	let system_event: <T as frame_system::Config>::Event = generic_event.into();
+	// compare to the last event record
+	let EventRecord { event, .. } = &events[events.len() - 1];
+	assert_eq!(event, &system_event);
+}
