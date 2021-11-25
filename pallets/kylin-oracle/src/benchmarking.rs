bd632e6c7129695729497f83527648cf2c064e86
Logan Bonsignore <Loganbon14@gmail.com>
2021-11-25 11:19:41 -0600

    add query benchmark

--- pallets/kylin-oracle/src/benchmarking.rs
+++ pallets/kylin-oracle/src/benchmarking.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "runtime-benchmarks")]
 use super::*;
-use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::{EventRecord, RawOrigin};
 
 benchmarks! {
@@ -38,14 +38,28 @@ benchmarks! {
         );
     }
 
-    // query_data {
-    //     let a in 1 .. 100;
-    //     let caller: T::AccountId = whitelisted_caller();
-    //     let feed_name = str::from_utf8(b"test_feed").unwrap().as_bytes().to_vec();
-    //     let data = b"{'benchmark':'this is data'}".to_vec();
-    //     let _ = Pallet::<T>::write_data_onchain(RawOrigin::Signed(caller.clone()).into(), feed_name.clone(), data);
-    //     let new_feed_name = str::from_utf8(b"custom_").unwrap().to_owned() + str::from_utf8(&feed_name).unwrap();
-    // }: _(RawOrigin::Signed(caller.clone()), None, new_feed_name.as_bytes().to_vec())
+    query_data {
+        let a in 1 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+        let feed_name = str::from_utf8(b"custom_benchmark_feed").unwrap().as_bytes().to_vec();
+        let data = b"{'benchmark':'this is data'}".to_vec();
+        // Submit data signed
+        let key: u64 = a.into();
+        let block_number = <system::Pallet<T>>::block_number();
+        let data = str::from_utf8(b"{'benchmark':'this is data'}").unwrap().as_bytes().to_vec();
+        <Pallet<T>>::submit_data_signed(RawOrigin::Signed(caller.clone()).into(), block_number, key, data.clone()).unwrap();
+        // Clear_processed_requests_unsigned 
+        <Pallet<T>>::clear_processed_requests_unsigned(RawOrigin::None.into(), block_number, [key].to_vec()).unwrap();
+        // Clear api queue
+        <Pallet<T>>::clear_api_queue_unsigned(RawOrigin::None.into(), block_number, [key].to_vec()).unwrap();
+        // Add fresh data
+        <Pallet<T>>::submit_data_signed(RawOrigin::Signed(caller.clone()).into(), block_number, key, data).unwrap();
+        // Insert new key to feed account lookup
+        let hash: Vec<u8> = str::from_utf8(b"5F28xL42VWThNonDft4TAQ6rw6a82E2jMsQXS5uMyKiA4ccv").unwrap().as_bytes().to_vec();
+        <FeedAccountLookup<T>>::insert(feed_name.clone(), (caller.clone(), hash));
+        // Get new feed name
+        // let new_feed_name = str::from_utf8(b"custom_").unwrap().to_owned() + str::from_utf8(&feed_name).unwrap();
+    }: _(RawOrigin::Signed(caller), None, feed_name)
 
     write_data_onchain {
         let a in 1 .. 100;
