9bbc4d1a8c858652c34de84ca7c246a5b2df1cfe
inodelisia <inodelisia@gmail.com>
2022-10-06 12:13:39 +0000

    spec_version correction for pichiu runtime

--- runtime/pichiu/src/lib.rs
+++ runtime/pichiu/src/lib.rs
@@ -116,7 +116,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("pichiu"),
 	impl_name: create_runtime_str!("pichiu"),
 	authoring_version: 1,
-	spec_version: 23,
+	spec_version: 25,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
