6f71b0e9b76bbabed5bab16d5691ca8f529f9aa4
inodelisia <inodelisia@gmail.com>
2022-11-02 14:01:39 +0000

    creation of the distribution pallet

--- pallets/kylin-distribution/src/models.rs
+++ pallets/kylin-distribution/src/models.rs
@@ -5,26 +5,26 @@ use kylin_support::types::{
 use scale_info::TypeInfo;
 use sp_runtime::{MultiSignature, RuntimeDebug};
 
-/// A single Airdrop.
+/// A single Distribution.
 #[derive(Encode, Decode, PartialEq, Eq, Copy, Clone, TypeInfo, MaxEncodedLen)]
-pub struct Airdrop<AccountId, Balance, Moment> {
-	/// Creator of the Airdrop.
+pub struct Distribution<AccountId, Balance, Moment> {
+	/// Creator of the Distribution.
 	pub creator: AccountId,
-	/// Total funds committed to the Airdrop.
+	/// Total funds committed to the Distribution.
 	pub total_funds: Balance,
 	/// Total number of recipients
 	pub total_recipients: u32,
 	/// Amount of the `total_funds` already claimed.
 	pub claimed_funds: Balance,
-	/// Starting block of the Airdrop.
+	/// Starting block of the Distribution.
 	pub start: Option<Moment>,
 	/// The minimum time, in blocks, between recipient claims.
 	pub schedule: Moment,
-	/// Set `true` if an airdrop has been explicitly disabled.
+	/// Set `true` if an distribution has been explicitly disabled.
 	pub disabled: bool,
 }
 
-/// Funds, and related information, to be claimed by an Airdrop recipient.
+/// Funds, and related information, to be claimed by an Distribution recipient.
 #[derive(Encode, Decode, PartialEq, Eq, Copy, Clone, TypeInfo, MaxEncodedLen)]
 pub struct RecipientFund<Balance, Period> {
 	/// Total funds committed for this recipient.
@@ -37,14 +37,14 @@ pub struct RecipientFund<Balance, Period> {
 	pub funded_claim: bool,
 }
 
-/// Current State of an [`Airdrop`](Airdrop).
+/// Current State of an [`Distribution`](Distribution).
 #[derive(Debug, Encode, Decode, PartialEq, Eq, Copy, Clone, TypeInfo, MaxEncodedLen)]
-pub enum AirdropState {
-	/// The Airdrop has been created but has not started.
+pub enum DistributionState {
+	/// The Distribution has been created but has not started.
 	Created,
-	/// The Airdrop has started. Recipients can claim funds.
+	/// The Distribution has started. Recipients can claim funds.
 	Enabled,
-	/// The Airdrop has ended. Recipients can **NOT** claim funds.
+	/// The Distribution has ended. Recipients can **NOT** claim funds.
 	Disabled,
 }
 
--- pallets/kylin-distribution/src/weights.rs
+++ pallets/kylin-distribution/src/weights.rs
@@ -1,16 +1,16 @@
 use frame_support::weights::Weight;
 
 pub trait WeightInfo {
-	fn create_airdrop() -> Weight;
+	fn create_distribution() -> Weight;
 	fn add_recipient(x: u32) -> Weight;
 	fn remove_recipient() -> Weight;
-	fn enable_airdrop() -> Weight;
-	fn disable_airdrop() -> Weight;
+	fn enable_distribution() -> Weight;
+	fn disable_distribution() -> Weight;
 	fn claim(x: u32) -> Weight;
 }
 
 impl WeightInfo for () {
-	fn create_airdrop() -> Weight {
+	fn create_distribution() -> Weight {
 		0
 	}
 
@@ -22,11 +22,11 @@ impl WeightInfo for () {
 		0
 	}
 
-	fn enable_airdrop() -> Weight {
+	fn enable_distribution() -> Weight {
 		0
 	}
 
-	fn disable_airdrop() -> Weight {
+	fn disable_distribution() -> Weight {
 		0
 	}
 
